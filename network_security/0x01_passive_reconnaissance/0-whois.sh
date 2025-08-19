2c9105ec7d1d9f10dc1038636ed462cd88da57d9
sabirgz <sabirgz@code.edu.az>
2025-08-20 01:00:55 +0400

    fix

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F: '/Registrant|Admin|Tech/ {print $1 "," $2}'
+whois $1 | awk -F': ' '/^(Registrant|Admin|Tech) (Organization|State\/Province|Country|Email):/ {print $1 "," $2}'
